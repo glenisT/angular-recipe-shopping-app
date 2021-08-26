bf7156c7dac3756c5c4538ee169bc71e833c0b18
glenisT <glentivari@gmail.com>
2021-08-26 14:00:17 +0200

    Finish recipe-detail component HTML skeleton

--- angular-recipe-shopping/src/app/recipes/recipe-list/recipe-list.component.ts
+++ angular-recipe-shopping/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,10 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
 
   recipes : Recipe[] = [
-    new Recipe('Test Recipe', 'Not a real recipe', 'https://media.istockphoto.com/vectors/recipe-book-vector-card-vector-id488269398')
+    new Recipe('Test Recipe', 'Not a real recipe.', 'https://media.istockphoto.com/vectors/recipe-book-vector-card-vector-id488269398'),
+    new Recipe('Chicken Fingers', 'I like chimkend.', 'https://www.uppercrustent.com/uce-content/uploads/2013/11/Panko-Chicken-Fingers.jpg'),
+    new Recipe('Medium Rare Steak', 'Beef good.', 'https://www.tasteofhome.com/wp-content/uploads/2019/01/medium-rare-steak-shutterstock_706040446.jpg'),
+
   ];
 
   constructor() { }
